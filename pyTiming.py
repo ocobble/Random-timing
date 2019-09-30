ea8c976e7c6758c94ce4aa733457ebfd9de07160
Brady Reynolds <breynolds376@ou.edu>
2019-09-30 18:28:56 -0500

    updated pyTiming.py

--- pyTiming.py
+++ pyTiming.py
@@ -16,17 +16,24 @@ then return in the format:
 so I can rn through the given input until I hit and @, then take in the rest of the imput as a string and run the code
 below on it. for the output, I need to give the entire output back, with the new information
 '''
-
+print("\n START: \n")
 for i in range(1, len( sys.argv)):
     strlist = str.split(sys.argv[i], '@')
     start = time.time()
     if(strlist[1][:4] != "http"):
         strlist[1] = ("http://" + strlist[1])
-    output = urllib2.urlopen(strlist[1])
+    try:
+        output = urllib2.urlopen(strlist[1],timeout =  5)
+    except urllib2.URLError as e:
+        print("got an error on " + strlist[1] + " of " + str(e.reason))
+        continue
     html = output.read()
     end = time.time()
+    number = ''.join(i for i in html if i.isdigit())
     formatStrings = str.split(strlist[0], "_")
-    print(strlist[1] + " : " + formatStrings[0] + "_" + formatStrings[1] + "_" + formatStrings[2] + "_" + formatStrings[3] + "@" + strlist[1] + " " + str(end - start) + " " +  str(html))
+    print(strlist[1] + " : " + formatStrings[0] + "_" + formatStrings[1] + "_" + formatStrings[2] + "_" + formatStrings[3] + "@" + strlist[1] + " " + str(end - start) + " " +  str(number))
     #print(sys.argv[i] + " returned a number " + str(html) +  " in " + str(end - start) + " seconds")
     #print(html)
+
+print("\n FINISH \n")
 
